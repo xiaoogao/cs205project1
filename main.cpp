d47b6e6a3fa81e1a4fd0c27dc3afe04422e72811
Xiao Gao <xiaogao@Xiaos-MacBook-Pro-2.local>
2025-05-08 00:03:55 -0700

    Complete heurstic function and A star search

--- main.cpp
+++ main.cpp
@@ -118,6 +118,7 @@ void print_state(const vector<int>& state) {
     std::cout << std::endl;
 }
 
+
 // Uniform Cost Search algorithm with h(n) hardcoded to 0
 void uniform_cost_search(Problem& problem) {
     priority_queue<Node> frontier;  // min-heap priority queue for UCS
@@ -145,15 +146,13 @@ void uniform_cost_search(Problem& problem) {
             return; // end
         }else
         {nodes_expanded++;} // increments the counter
+        
         explored.insert(node.state); // adds current node state to the explored set
 
         // get possible moves and expand nodes
         vector <vector<string>> possible_moves = problem.get_possible_moves(node.state); // store all valid moves from current state
-        
 
-        for (size_t i = 0; i < possible_moves.size(); ++i) { // iterate through each blank position
-            
-            
+        for (size_t i = 0; i < possible_moves.size(); ++i) { // iterate through each blank position      
             for (size_t j = 0; j < possible_moves[i].size(); j++) // iterate through each possible move for ith blank position
             {
                 const string& move = possible_moves[i][j]; // get one move for the selected blank
@@ -191,151 +190,153 @@ void uniform_cost_search(Problem& problem) {
 }
 
 // Calculate the heuristic cost
-double Calheuristic(const vector<int>& state){
+double Calheuristic(const vector<int>& state, const vector<int>& goal){
+
+    // //double euclidean = 0;
+    // int manhattan = 0;
+
+    // auto it = find(state.begin(), state.end(), 1); 
+    // int index = distance(state.begin(), it); // get the index of 1
+
+    // int row = 0;
+    // int column = 0; // initial the row number and column number of 1
+    
+    // if(index <= 9){ // 1 is in the bottom row
+    //     column = index % 10;
+    // }else if (index == 10){ // case when 1 is in the first block on the top row
+    //     row = 1;
+    //     column = 3;
+    // }else if (index == 11){ // case when 1 is in the second block on the top row
+    //     row = 1;
+    //     column = 5;
+    // }else if (index == 12){ // case when 1 is in the thrid block on the top row
+    //     row = 1;
+    //     column = 7;
+    // } 
+
+    // int dx = column - 0;
+    // int dy = row - 0;
+    // manhattan = abs(dx) + abs(dy);
 
     //double euclidean = 0;
+
     int manhattan = 0;
-
-    auto it = find(state.begin(), state.end(), 1); 
-    int index = distance(state.begin(), it); // get the index of 1
-
-    int row = 0;
-    int column = 0; // initial the row number and column number of 1
+    int dis = 0;
     
-    if(index <= 9){ // 1 is in the bottom row
-        column = index % 10;
-    }else if (index == 10){ // case when 1 is in the first block on the top row
-        row = 1;
-        column = 3;
-    }else if (index == 11){ // case when 1 is in the second block on the top row
-        row = 1;
-        column = 5;
-    }else if (index == 12){ // case when 1 is in the thrid block on the top row
-        row = 1;
-        column = 7;
-    } 
+    for (size_t i=0; i < state.size(); ++i){ // iterate through the state vector
+        if(state[i] != 0 && state[i] != goal[i]){
+            auto it = find(goal.begin(), goal.end(), state[i]); 
+            int index = distance(goal.begin(), it); // get the index in the goal
+            
+            int row1 = 0;
+            int row2 = 0;
+            int column1 = 0;
+            int column2 = 0;
 
-    int dx = column - 0;
-    int dy = row - 0;
-    manhattan = abs(dx) + abs(dy);
+            if(index <= 9){ // find the row and column in the goal state
+                row1 = 0;
+                column1 = index % 10;
+            }else if (index == 10){ // case when sergeant is in the first block on the top row
+                row1 = 1;
+                column1 = 3;
+            }else if (index == 11){ // case when sergeant is in the second block on the top row
+                row1 = 1;
+                column1 = 5;
+            }else if (index == 12){ // case when sergeant is in the thrid block on the top row
+                row1 = 1;
+                column1 = 7;
+            }
+
+            if(i <= 9){ // find the row and column in the current state
+                row2 = 0;
+                column2 = i % 10;
+            }else if (i == 10){ // case when sergeant is in the first block on the top row
+                row2 = 1;
+                column2 = 3;
+            }else if (i == 11){ // case when sergeant is in the second block on the top row
+                row2 = 1;
+                column2 = 5;
+            }else if (i == 12){ // case when sergeant is in the thrid block on the top row
+                row2 = 1;
+                column2 = 7;
+            }
+
+            int dx = column1 - column2;
+            int dy = row1 - row2;
+            dis = abs(dx) + abs(dy); // find the manhattan distance for each misplaced sergeant
+            
+            manhattan = manhattan + dis; // find the sum of manhattan distan for all misplaced sergeant
+        }
+
+    }
 
     return manhattan;
 }
 
-// Heuristic search use misplaced tiles cost
-// x = 3 to use euclidean distance cost
-void misplaced_heuristic_search(Problem& problem){
-    // priority_queue<Node> frontier;
-    // set<vector<int> > explored;
+// Heuristic search use manhattan distance cost
+void manhattan_heuristic_search(Problem& problem){
+    priority_queue<Node> frontier;
+    set<vector<int> > explored;
 
-    // double initial_heuristic = Calheuristic(problem.initial_state, problem.goal_state, 2);
-    // frontier.push(Node(problem.initial_state, nullptr, "", 0, initial_heuristic));
-    // int nodes_expanded = 0;
-    // int max_queue_size = 1;
+    double initial_heuristic = Calheuristic(problem.initial_state, problem.goal_state);
+    frontier.push(Node(problem.initial_state, nullptr, "", 0, initial_heuristic));
+    int nodes_expanded = 0;
+    int max_queue_size = 1;
 
-    //  while (!frontier.empty()) {
-    //     Node node = frontier.top();
-    //     frontier.pop();
+    while (!frontier.empty()) {
+        Node node = frontier.top();
+        frontier.pop();
 
-    //     cout << "Expanding state: \n";
-    //     if (nodes_expanded > 0)
-    //     {
-    //         cout << "The best state to expand with g(n) = " << node.path_cost << " and h(n) = " << node.heuristic_cost << " is...";
-    //     }
-    //     print_state(node.state);
+        cout << "Expanding state: \n";
 
-    //     if (problem.is_goal(node.state)) {
-    //         cout << "Goal!!!" << endl;
-    //         cout << "Solution found at depth: " << node.path_cost << endl;
-    //         cout << "Nodes expanded: " << nodes_expanded << endl;
-    //         cout << "Maximum queue size: " << max_queue_size << endl;
-    //         return;
-    //     }else
-    //     {
-    //         nodes_expanded++;
-    //     }
+        if (nodes_expanded > 0)
+        {
+            cout << "The best state to expand with g(n) = " << node.path_cost << " and h(n) = " << node.heuristic_cost << " is..." << endl;
+        }
+        print_state(node.state);
+
+        if (problem.is_goal(node.state)) {
+            cout << "Goal!!!" << endl;
+            cout << "Solution found at depth: " << node.path_cost << endl;
+            cout << "Nodes expanded: " << nodes_expanded << endl;
+            cout << "Maximum queue size: " << max_queue_size << endl;
+            return;
+        }else
+        {
+            nodes_expanded++;
+        }
+        explored.insert(node.state);
+
+        // std::cout << "Explored Nodes:" <<std::endl;
+        // for(size_t i = 0; i < explored.size(); ++i){     
+        //     std::set< vector<int> >::iterator it = explored.begin();
+        //     std::advance(it, i);
+        //     print_state(*it);
+        // }
         
-
-    //     explored.insert(node.state);
-
-    //     vector<string> possible_moves = problem.get_possible_moves(node.state);
-    //     // cout << possible_moves[1];
-    //     for (size_t i = 0; i < possible_moves.size(); i++) {
-    //         const string& move = possible_moves[i];
+        // get possible moves and expand nodes
+        vector <vector<string>> possible_moves = problem.get_possible_moves(node.state); // store all valid moves from current state
+        for (size_t i = 0; i < possible_moves.size(); ++i) { // iterate through each blank position      
+            for (size_t j = 0; j < possible_moves[i].size(); j++) // iterate through each possible move for ith blank position
+            {
+                const string& move = possible_moves[i][j]; // get one move for the selected blank
+                vector<int> new_state = problem.apply_operator(node.state, i, move); // aply the move to the selected blank and returns a new state
             
-    //         vector<int> new_state = problem.apply_operator(node.state, move);
-
-    //         if (explored.find(new_state) == explored.end()) {
-    //             double new_heuristic = Calheuristic(new_state, problem.goal_state, 2);
-    //             Node child(new_state, &node, move, node.path_cost + 1, new_heuristic);
-    //             frontier.push(child);
-    //             // max_queue_size++;
-    //         }
-    //     }
-
-    //     max_queue_size = max(max_queue_size, (int)frontier.size());
-    // }
-
-    // cout << "No solution found." << endl;
-    // cout << "Nodes expanded: " << nodes_expanded << endl;
-    // cout << "Maximum queue size: " << max_queue_size << endl;
-}
-
-void euclidean_heuristic_search(Problem& problem){
-    // priority_queue<Node> frontier;
-    // set<vector<int> > explored;
-
-    // double initial_heuristic = Calheuristic(problem.initial_state, problem.goal_state, 3);
-    // frontier.push(Node(problem.initial_state, nullptr, "", 0, initial_heuristic));
-    // int nodes_expanded = 0;
-    // int max_queue_size = 1;
-
-    //  while (!frontier.empty()) {
-    //     Node node = frontier.top();
-    //     frontier.pop();
-
-    //     cout << "Expanding state: \n";
-    //     if (nodes_expanded > 0)
-    //     {
-    //         cout << "The best state to expand with g(n) = " << node.path_cost << " and h(n) = " << node.heuristic_cost << " is...";
-    //     }
-    //     print_state(node.state);
-
-    //     if (problem.is_goal(node.state)) {
-    //         cout << "Goal!!!" << endl;
-    //         cout << "Solution found at depth: " << node.path_cost << endl;
-    //         cout << "Nodes expanded: " << nodes_expanded << endl;
-    //         cout << "Maximum queue size: " << max_queue_size << endl;
-    //         return;
-    //     }else
-    //     {
-    //         nodes_expanded++;
-    //     }
-        
-
-    //     explored.insert(node.state);
-
-    //     vector<string> possible_moves = problem.get_possible_moves(node.state);
-    //     // cout << possible_moves[1];
-    //     for (size_t i = 0; i < possible_moves.size(); i++) {
-    //         const string& move = possible_moves[i];
-            
-    //         vector<int> new_state = problem.apply_operator(node.state, move);
-
-    //         if (explored.find(new_state) == explored.end()) {
-    //             double new_heuristic = Calheuristic(new_state, problem.goal_state, 3);
-    //             Node child(new_state, &node, move, node.path_cost + 1, new_heuristic);
-    //             frontier.push(child);
-    //             // max_queue_size++;
-    //         }
-    //     }
-
-    //     max_queue_size = max(max_queue_size, (int)frontier.size());
-    // }
-
-    // cout << "No solution found." << endl;
-    // cout << "Nodes expanded: " << nodes_expanded << endl;
-    // cout << "Maximum queue size: " << max_queue_size << endl;
+                // check if the new state has already been explored
+                if (explored.find(new_state) == explored.end()) { // if not then create a new node for it, if in the explored set then skip
+                    
+                    double new_heuristic = Calheuristic(new_state, problem.goal_state);
+                    int new_path_cost = node.path_cost + 1; // increment g(n), path cost
+                    Node child(new_state, &node, move, new_path_cost, new_heuristic); // h(n) = 0 for UCS
+                    frontier.push(child); //adds the child node to the frontier
+                }
+            }
+        }
+        max_queue_size = max(max_queue_size, (int)frontier.size());
+    }
+    cout << "No solution found." << endl;
+    cout << "Nodes expanded: " << nodes_expanded << endl;
+    cout << "Maximum queue size: " << max_queue_size << endl;
 }
 
 int main() {
@@ -344,19 +345,19 @@ int main() {
     cout << "This is Xiao's puzzle solver for nine man in a trench problem." << endl;
     cout << "The initial state is " << endl;
     // Set the default initial puzzle state
-    initial_state[0] = 0;
+    initial_state[0] = 1;
     initial_state[1] = 2;
     initial_state[2] = 3;
-    initial_state[3] = 4;
-    initial_state[4] = 5;
-    initial_state[5] = 6;
-    initial_state[6] = 7;
-    initial_state[7] = 8;
-    initial_state[8] = 9;
-    initial_state[9] = 1;
+    initial_state[3] = 0;
+    initial_state[4] = 0;
+    initial_state[5] = 0;
+    initial_state[6] = 5;
+    initial_state[7] = 6;
+    initial_state[8] = 8;
+    initial_state[9] = 9;
     initial_state[10] = 0;
-    initial_state[11] = 0;
-    initial_state[12] = 0;
+    initial_state[11] = 4;
+    initial_state[12] = 7;
     print_state(initial_state);
 
  
@@ -379,8 +380,7 @@ int main() {
     int methodChoice;
     cout << "Enter your choice of algorithm" << endl;
     cout << "1) Uniform Cost Search" << endl;
-    cout << "2) A* with the Misplaced Tile heuristic." << endl;
-    cout << "3) A* with the Euclidean distance heuristic." << endl;  
+    cout << "2) A* with the Manhattan Distance heuristic." << endl;
     cin >> methodChoice;
 
     // Create a problem instance
@@ -392,11 +392,7 @@ int main() {
         uniform_cost_search(puzzle_problem);
     }else if (methodChoice == 2)
     {
-        misplaced_heuristic_search(puzzle_problem);
-
-    }else if (methodChoice == 3)
-    {
-        euclidean_heuristic_search(puzzle_problem);
+        manhattan_heuristic_search(puzzle_problem);
     }
 
     return 0;
